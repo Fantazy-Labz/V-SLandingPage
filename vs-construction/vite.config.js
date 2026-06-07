9d77a4998e425a4ea40ac77e8052e38101864e5a
salog0d <david.salomon.nava11@gmail.com>
2026-06-07 11:07:46 -0600

    fixed hero image path

--- vs-construction/vite.config.js
+++ vs-construction/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Rutas relativas para servir desde un subpath de bucket GCS
+  // (https://storage.googleapis.com/TU-BUCKET/)
+  base: './',
   plugins: [react()],
 })
